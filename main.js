4bb6a6b5219586f343950ebde07767a777c07321
Sina Mohammadian <34884156+Sinamh@users.noreply.github.com>
2022-04-25 16:26:23 +0430

    created first page

---
+++ main.js
@@ -0,0 +1,24 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow } = electron;
+// const baseUrl = new URL("");
+
+let mainWindow;
+
+// Listen for the app to be ready
+app.on("ready", function () {
+  // Create a new window
+  mainWindow = new BrowserWindow();
+  // Load html into the window
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "main-window.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
+  // url.pathToFileURL(path.join(__dirname, "main-window.html"));
+});
+// console.log(url.pathToFileURL(path.join(__dirname, "main-window.html")).href);
