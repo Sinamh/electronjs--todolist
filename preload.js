bf773321973f85f70899451d51aca7277af0825d
Sina Mohammadian <34884156+sinamhdn@users.noreply.github.com>
2023-05-16 08:50:54 +0330

    Update preload.js

--- preload.js
+++ preload.js
@@ -8,6 +8,7 @@ const {
   closeWindow,
 } = require("./menu/menu-functions");
 
+// to access menu options in frontend add them to window object
 window.addEventListener("DOMContentLoaded", () => {
   window.getCurrentWindow = getCurrentWindow;
   window.openMenu = openMenu;
